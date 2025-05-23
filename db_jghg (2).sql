d284a0f81e4778736189626d61d0b9293c0675d5
Leonardo-b2 <valencialeonardo905@gmail.com>
2025-05-23 09:48:41 -0500

    base de datos

---
+++ db_jghg (2).sql
@@ -0,0 +1,622 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3305
+-- Tiempo de generación: 23-05-2025 a las 16:47:55
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_jghg`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_login_por_usuario` (IN `p_login` VARCHAR(50))   BEGIN
+    SELECT 
+        u.id_usuario, 
+        u.login, 
+        u.clave, 
+        u.cargo, 
+        u.estado, 
+        u_p.id_permiso, 
+        p.nom_permiso
+    FROM usuario u
+    INNER JOIN usuario_permiso u_p ON u.id_usuario = u_p.id_usuario
+    INNER JOIN permiso p ON u_p.id_permiso = p.id_permiso
+    WHERE u.login = p_login;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_permisos_por_usuario` (IN `p_id_usuario` INT)   BEGIN
+    SELECT 
+        u.id_usuario, 
+        u_p.id_permiso, 
+        p.nom_permiso
+    FROM usuario u
+    INNER JOIN usuario_permiso u_p ON u.id_usuario = u_p.id_usuario
+    INNER JOIN permiso p ON u_p.id_permiso = p.id_permiso
+    WHERE u_p.id_usuario = p_id_usuario;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usuario_buscar_por_id` (IN `p_id` INT)   BEGIN
+    SELECT * FROM usuario WHERE id_usuario = p_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usuario_buscar_por_usuario` (IN `p_login` VARCHAR(50))   BEGIN
+    SELECT 
+        u.id_usuario, 
+        u.nom_usuario, 
+        per.tipo_documento, 
+        per.num_identidad, 
+        per.direccion, 
+        per.telefono, 
+        u.login, 
+        u.cargo, 
+        u.imagen, 
+        u.estado, 
+        u_p.id_permiso, 
+        p.nom_permiso
+    FROM usuario u
+    INNER JOIN usuario_permiso u_p ON u.id_usuario = u_p.id_usuario
+    INNER JOIN permiso p ON u_p.id_permiso = p.id_permiso
+    INNER JOIN persona per ON per.id_persona = u.id_persona
+    WHERE u.login = p_login;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usuario_todas` ()   BEGIN
+    SELECT * FROM usuario u INNER JOIN usuario_permiso u_p ON u.id_usuario=u_p.id_usuario;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `id_alumno` int(10) UNSIGNED NOT NULL,
+  `id_persona` int(10) UNSIGNED NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp(),
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+CREATE TABLE `asignatura` (
+  `id_asignatura` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `id_departamento` int(10) UNSIGNED NOT NULL,
+  `nom_departamento` varchar(100) NOT NULL,
+  `cod_departamento` char(2) NOT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `departamento`
+--
+
+INSERT INTO `departamento` (`id_departamento`, `nom_departamento`, `cod_departamento`, `estado`) VALUES
+(1, 'ica', '01', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `distrito`
+--
+
+CREATE TABLE `distrito` (
+  `id_distrito` int(10) UNSIGNED NOT NULL,
+  `id_provincia` int(11) UNSIGNED NOT NULL,
+  `nom_distrito` varchar(100) NOT NULL,
+  `cod_distrito` char(2) NOT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `distrito`
+--
+
+INSERT INTO `distrito` (`id_distrito`, `id_provincia`, `nom_distrito`, `cod_distrito`, `estado`) VALUES
+(1, 1, 'ica', '01', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente`
+--
+
+CREATE TABLE `docente` (
+  `id_docente` int(10) UNSIGNED NOT NULL,
+  `id_persona` int(10) UNSIGNED NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `especialidad` varchar(100) DEFAULT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo`
+--
+
+CREATE TABLE `grupo` (
+  `id_grupo` int(10) UNSIGNED NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `nom_grupo` varchar(50) NOT NULL,
+  `favorito` char(1) NOT NULL DEFAULT '0',
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo_alumno`
+--
+
+CREATE TABLE `grupo_alumno` (
+  `id_grupo_alumno` int(10) UNSIGNED NOT NULL,
+  `id_grupo` int(10) UNSIGNED NOT NULL,
+  `id_alumno` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matricula`
+--
+
+CREATE TABLE `matricula` (
+  `id_matricula` int(10) UNSIGNED NOT NULL,
+  `id_alumno` int(10) UNSIGNED NOT NULL,
+  `id_asignatura` int(10) UNSIGNED NOT NULL,
+  `id_grupo` int(10) UNSIGNED NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nota`
+--
+
+CREATE TABLE `nota` (
+  `id_nota` int(10) UNSIGNED NOT NULL,
+  `id_matricula` int(10) UNSIGNED NOT NULL,
+  `unidad` int(11) NOT NULL,
+  `nota` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permiso`
+--
+
+CREATE TABLE `permiso` (
+  `id_permiso` int(10) UNSIGNED NOT NULL,
+  `nom_permiso` varchar(50) NOT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `permiso`
+--
+
+INSERT INTO `permiso` (`id_permiso`, `nom_permiso`, `estado`) VALUES
+(1, 'escritorio', '1'),
+(2, 'gestion_usuario', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id_persona` int(10) UNSIGNED NOT NULL,
+  `id_distrito` int(11) UNSIGNED NOT NULL,
+  `nombres` varchar(150) NOT NULL,
+  `apellido_pa` varchar(50) NOT NULL,
+  `apellido_ma` varchar(50) NOT NULL,
+  `tipo_documento` char(2) NOT NULL,
+  `num_identidad` char(8) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `telefono` char(9) DEFAULT NULL,
+  `direccion` varchar(150) DEFAULT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id_persona`, `id_distrito`, `nombres`, `apellido_pa`, `apellido_ma`, `tipo_documento`, `num_identidad`, `email`, `telefono`, `direccion`, `estado`) VALUES
+(1, 1, 'admin', 'ape_admin', 'ape2_admin', 'DN', '17140911', 'admin@gmail.com', '933281321', 'Ica', '1'),
+(2, 1, 'director', 'ape_director', 'ape2_director', 'DN', '33340911', 'director@jghg.edu.pe', '931233132', 'Ica sur', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincia`
+--
+
+CREATE TABLE `provincia` (
+  `id_provincia` int(11) UNSIGNED NOT NULL,
+  `id_departamento` int(10) UNSIGNED NOT NULL,
+  `nom_provincia` varchar(100) NOT NULL,
+  `cod_provincia` char(2) NOT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `provincia`
+--
+
+INSERT INTO `provincia` (`id_provincia`, `id_departamento`, `nom_provincia`, `cod_provincia`, `estado`) VALUES
+(1, 1, 'ica', '01', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `id_persona` int(10) UNSIGNED NOT NULL,
+  `nom_usuario` varchar(50) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `clave` varchar(100) NOT NULL,
+  `cargo` enum('admin','docente','otro') NOT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `id_persona`, `nom_usuario`, `login`, `clave`, `cargo`, `imagen`, `estado`) VALUES
+(1, 1, 'Admin00', 'admin@jghg.edu.pe', '$2y$10$bJ84ra.xyEZEEMCwMON0G.zxJDH3jYmtLVo2cbqo9dLHD64Bu31Ba', 'admin', '1714091104.png', '1'),
+(2, 2, 'Director', 'director@jghg.edu.pe', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e99', 'docente', '1714091104.png', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_permiso`
+--
+
+CREATE TABLE `usuario_permiso` (
+  `id_usuario_permiso` int(10) UNSIGNED NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `id_permiso` int(10) UNSIGNED NOT NULL,
+  `estado` char(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario_permiso`
+--
+
+INSERT INTO `usuario_permiso` (`id_usuario_permiso`, `id_usuario`, `id_permiso`, `estado`) VALUES
+(1, 1, 1, '1'),
+(2, 1, 2, '1'),
+(3, 2, 1, '1'),
+(4, 2, 2, '1');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id_alumno`),
+  ADD UNIQUE KEY `id_persona` (`id_persona`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD PRIMARY KEY (`id_asignatura`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id_departamento`),
+  ADD UNIQUE KEY `cod_departamento_UQ_DE` (`cod_departamento`),
+  ADD UNIQUE KEY `nom_departamento_UQ_DE` (`nom_departamento`);
+
+--
+-- Indices de la tabla `distrito`
+--
+ALTER TABLE `distrito`
+  ADD PRIMARY KEY (`id_distrito`),
+  ADD UNIQUE KEY `cod_distrito_UQ_DIS` (`cod_distrito`),
+  ADD KEY `id_provincia_FK_DIS` (`id_provincia`);
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`id_docente`),
+  ADD UNIQUE KEY `id_persona` (`id_persona`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD PRIMARY KEY (`id_grupo`),
+  ADD UNIQUE KEY `nom_grupo` (`nom_grupo`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `grupo_alumno`
+--
+ALTER TABLE `grupo_alumno`
+  ADD PRIMARY KEY (`id_grupo_alumno`),
+  ADD KEY `id_grupo` (`id_grupo`),
+  ADD KEY `id_alumno` (`id_alumno`);
+
+--
+-- Indices de la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  ADD PRIMARY KEY (`id_matricula`),
+  ADD KEY `id_alumno` (`id_alumno`),
+  ADD KEY `id_asignatura` (`id_asignatura`),
+  ADD KEY `id_grupo` (`id_grupo`);
+
+--
+-- Indices de la tabla `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`id_nota`),
+  ADD KEY `id_matricula` (`id_matricula`);
+
+--
+-- Indices de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  ADD PRIMARY KEY (`id_permiso`),
+  ADD UNIQUE KEY `nom_permiso` (`nom_permiso`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id_persona`),
+  ADD UNIQUE KEY `num_identidad` (`num_identidad`),
+  ADD UNIQUE KEY `telefono_UQ_PE` (`telefono`),
+  ADD UNIQUE KEY `email_UQ_PER` (`email`),
+  ADD KEY `id_distrito_FK_P` (`id_distrito`);
+
+--
+-- Indices de la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  ADD PRIMARY KEY (`id_provincia`),
+  ADD UNIQUE KEY `cod_provincia_UQ_PROV` (`cod_provincia`),
+  ADD KEY `id_departamento_FK_PROV` (`id_departamento`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `nom_usuario` (`nom_usuario`),
+  ADD UNIQUE KEY `login` (`login`),
+  ADD KEY `id_persona` (`id_persona`);
+
+--
+-- Indices de la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  ADD PRIMARY KEY (`id_usuario_permiso`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_permiso` (`id_permiso`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id_alumno` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id_asignatura` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `id_departamento` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `distrito`
+--
+ALTER TABLE `distrito`
+  MODIFY `id_distrito` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `docente`
+--
+ALTER TABLE `docente`
+  MODIFY `id_docente` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  MODIFY `id_grupo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupo_alumno`
+--
+ALTER TABLE `grupo_alumno`
+  MODIFY `id_grupo_alumno` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  MODIFY `id_matricula` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `nota`
+--
+ALTER TABLE `nota`
+  MODIFY `id_nota` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  MODIFY `id_permiso` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id_persona` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  MODIFY `id_provincia` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  MODIFY `id_usuario_permiso` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
+  ADD CONSTRAINT `alumno_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `distrito`
+--
+ALTER TABLE `distrito`
+  ADD CONSTRAINT `id_provincia_FK_DIS` FOREIGN KEY (`id_provincia`) REFERENCES `provincia` (`id_provincia`);
+
+--
+-- Filtros para la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD CONSTRAINT `docente_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
+  ADD CONSTRAINT `docente_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `grupo_alumno`
+--
+ALTER TABLE `grupo_alumno`
+  ADD CONSTRAINT `grupo_alumno_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`),
+  ADD CONSTRAINT `grupo_alumno_ibfk_2` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`);
+
+--
+-- Filtros para la tabla `matricula`
+--
+ALTER TABLE `matricula`
+  ADD CONSTRAINT `matricula_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  ADD CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`),
+  ADD CONSTRAINT `matricula_ibfk_3` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`);
+
+--
+-- Filtros para la tabla `nota`
+--
+ALTER TABLE `nota`
+  ADD CONSTRAINT `nota_ibfk_1` FOREIGN KEY (`id_matricula`) REFERENCES `matricula` (`id_matricula`);
+
+--
+-- Filtros para la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `id_distrito_FK_P` FOREIGN KEY (`id_distrito`) REFERENCES `distrito` (`id_distrito`);
+
+--
+-- Filtros para la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  ADD CONSTRAINT `id_departamento_FK_PROV` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id_departamento`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`);
+
+--
+-- Filtros para la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  ADD CONSTRAINT `usuario_permiso_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `usuario_permiso_ibfk_2` FOREIGN KEY (`id_permiso`) REFERENCES `permiso` (`id_permiso`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
